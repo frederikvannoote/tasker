5609d290ad8dfe962dcc970a993c896e8e80d32f
frederik vannoote <frederik.vannoote@barco.com>
2020-08-30 17:36:57 +0200

    Add swiper actions to Task
    to right: i have executed the task
    to left: remove the task from the list.

--- app/src/main/java/net/vannoote/tasker/ListFragment.kt
+++ app/src/main/java/net/vannoote/tasker/ListFragment.kt
@@ -1,19 +1,28 @@
 package net.vannoote.tasker
 
 import android.content.Context
+import android.graphics.Canvas
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.FirebaseDatabase
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 import kotlinx.android.synthetic.main.fragment_list.view.*
 import net.vannoote.tasker.datamodel.Task
 import net.vannoote.tasker.datamodel.TaskAdapter
 import net.vannoote.tasker.datamodel.TaskParser
+import java.time.LocalDateTime
 
 
 /**
@@ -25,6 +34,7 @@ class ListFragment(taskerInteraction: TaskerInteraction) : Fragment() {
     private var visible: Boolean = false
     private var taskListAdaptor: TaskAdapter? = null
     private var mInteraction: TaskerInteraction = taskerInteraction
+    private var removedTask: Task? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -47,6 +57,79 @@ class ListFragment(taskerInteraction: TaskerInteraction) : Fragment() {
 
         taskListAdaptor = TaskAdapter(options)
         list.adapter = taskListAdaptor
+
+        val itemTouchHelperCallback =
+            object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                @RequiresApi(Build.VERSION_CODES.O)
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val position: Int = viewHolder.adapterPosition
+
+                    when {
+                        direction == ItemTouchHelper.LEFT -> {
+                            // Remove task
+                            removedTask = Task("id", "taskname", "daily", LocalDateTime.now())
+
+//                            FirebaseDatabase.getInstance().reference.child("tasks").
+
+                            taskListAdaptor!!.notifyItemRemoved(viewHolder.adapterPosition)
+
+                            Toast.makeText(context,
+                                "deleted",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            Snackbar.make(list, "Undo", Snackbar.LENGTH_LONG)
+                                .setAction("Undo", View.OnClickListener {
+                                    fun onClick(view: View) {
+                                        taskListAdaptor!!.notifyItemInserted(viewHolder.adapterPosition)
+
+                                    }
+                                }).show()
+                        }
+                        direction == ItemTouchHelper.RIGHT -> {
+                            Toast.makeText(context,
+                                "archived",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+//                    noteViewModel.delete(noteAdapter.getNoteAt(viewHolder.adapterPosition))
+
+                }
+
+                override fun onChildDraw(
+                    c: Canvas,
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    dX: Float,
+                    dY: Float,
+                    actionState: Int,
+                    isCurrentlyActive: Boolean
+                ) {
+                    RecyclerViewSwipeDecorator.Builder(context, c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                        .addSwipeLeftBackgroundColor(ContextCompat.getColor(context!!, R.color.removeTask))
+                        .addSwipeLeftActionIcon(R.drawable.ic_delete_task)
+                        .addSwipeRightBackgroundColor(ContextCompat.getColor(context!!, R.color.executeTask))
+                        .addSwipeRightActionIcon(R.drawable.ic_executed_task)
+                        .create()
+                        .decorate()
+
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                }
+
+            }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(list)
 
         // To 'Add' screen
         view.fab.setOnClickListener { mInteraction.showAddScreen() }
