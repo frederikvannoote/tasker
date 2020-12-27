20becf4eec72505697eef3760676107e2b175e7a
Frederik Vannoote <frederik.vannoote@barco.com>
2020-12-27 18:05:44 +0100

    Add tasklist data to database

--- app/src/main/java/net/vannoote/tasker/AddTaskListFragment.kt
+++ app/src/main/java/net/vannoote/tasker/AddTaskListFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_add.*
@@ -35,25 +36,43 @@ class AddTaskListFragment(taskerInteraction: TaskerInteraction) : Fragment() {
     }
 
     private fun addEntry(): String? {
+
+        var key: String? = null
+
+        // Add group
         val groups = getDatabaseRef()?.child("groups")
         if (groups != null) {
             var group = HashMap<String, String>()
-            group.put("displayname", tasklist_name.text.toString())
+            group["displayname"] = tasklistname.text.toString()
 
-            val key = groups.push().key
+            key = groups.push().key
             if (key == null) {
                 Log.w("Database", "Couldn't get push key for posts")
                 return null
             }
 
             val childUpdates = hashMapOf<String, Any>(
-                "/$key" to group
+                    "/$key" to group
             )
 
             groups.updateChildren(childUpdates)
-            return key
         }
-        return null
+
+        // Add user info
+        val users = getDatabaseRef()?.child("users")
+        if (users != null && key != null) {
+            var user = HashMap<String, String>()
+            user["group"] = key
+
+            val uid = FirebaseAuth.getInstance().currentUser?.uid
+            val childUpdates = hashMapOf<String, Any>(
+                    "/$uid" to user
+            )
+
+            users.updateChildren(childUpdates)
+        }
+
+        return key
     }
 
     private fun getDatabaseRef(): DatabaseReference? {
